1e8e3dd6b05f2771b96ca00ba5b39265b38f4a90
Samuel Robinson <samuel.vj.robinson@gmail.com>
2023-09-28 15:09:33 -0600

    Updated lecture 4, started on lecture 5

--- 04 GLMs/beeDatGLM.R
+++ 04 GLMs/beeDatGLM.R
@@ -1,11 +1,11 @@
-#Generate some bat data
+#Generate some bee data
 
 library(tidyverse)
 library(broom)
 theme_set(theme_classic())
 
-setwd("~/Documents/ecoStats2020/08 GLMs 2")
 set.seed(123)
+setwd('./04 GLMs')
 
 #Functions
 logit <- function(x) log(x/(1-x))
@@ -14,15 +14,15 @@ invLogit <- function(x) exp(x)/(1+exp(x))
 #Generate some non-normal data
 N <- 100
 var1 <- sample(c('Edmonton','Calgary'),N,replace=TRUE) #Sampling site
-var2 <- rexp(N,0.5) #Nest volume
+var2 <- rexp(N,0.5) #Floral resources
 
-dat <- data.frame(city=var1,size=var2)
-modMat <- model.matrix(~city+size,data=dat)
+dat <- data.frame(city=var1,floral=var2)
+modMat <- model.matrix(~city+floral,data=dat)
 coefs <- c(1,-0.2,0.3) #Same coefficients for both models
 
 yhat <- modMat %*% coefs #Expected value (link scale)
 
-dat$batCounts <- rpois(N,exp(yhat))  #Poisson process
-dat$batPres <- rbinom(N,1,invLogit(yhat))  #Binomial (bernoulli) process
+dat$beeCounts <- rpois(N,exp(yhat))  #Poisson process
+dat$beePres <- rbinom(N,1,invLogit(yhat))  #Binomial (bernoulli) process
 
-write.csv(dat,file = 'batDatGLM.csv')
+write.csv(dat,file = 'beeDatGLM.csv',row.names = FALSE)
